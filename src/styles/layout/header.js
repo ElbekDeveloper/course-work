a187582aa49a020e5fc24a6646a1770c8aa8b8b9
Elbek <e.normurodov@mail.ru>
2019-11-17 13:04:35 +0500

    feat: add fade-animation to menu-icon and nav-list

--- src/styles/layout/header.js
+++ src/styles/layout/header.js
@@ -3,10 +3,28 @@
 export const navSlide = () => {
     const menuIcon = document.querySelector(".menu-icon");
     const nav = document.querySelector(".nav-list");
+    const navLinks = document.querySelectorAll(".nav-list li");
+
 
     menuIcon.addEventListener("click", () => {
+        //Toggle nav-bar
         nav.classList.toggle("nav-active");
+
+        //Animate links 
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5 ease forwards ${index/7 + 2}s`;
+            }
+        });
+
+        //Menu-icon animation
+
+        menuIcon.classList.toggle("toggle");
     });
+
+
 }
 
 navSlide();
